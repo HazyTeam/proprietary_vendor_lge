58cf457d53cee36eb1f1db7b2ce815f172656fbb
Salvatore Festa <salvatorefestatech@gmail.com>
2016-04-06 14:39:21 +0200

    hammerhead: Update vendor to MMB29X

--- hammerhead/proprietary/Android.mk
+++ hammerhead/proprietary/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2013-2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ ifeq ($(TARGET_DEVICE),hammerhead)
 include $(CLEAR_VARS)
 LOCAL_MODULE := OmaDmclient
 LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
@@ -42,7 +42,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := SprintHiddenMenu
 LOCAL_MODULE_OWNER := lge
-LOCAL_SRC_FILES := priv-app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := app/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
